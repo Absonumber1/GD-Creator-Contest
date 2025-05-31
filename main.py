a58b8ee782764516819d1cfce0f0cc8004e22602
Absonumber1 <abdelrahmanadss2023@gmail.com>
2025-05-31 18:25:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ tree = bot.tree
 # Google Sheets Setup
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("google_credentials.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
 sheet = client.open("GD Creator Contest Submissions").sheet1
 
